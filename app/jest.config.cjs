a0107d69be6c4168f5fc85bc15dec987c1068c99
Katuyuki Kakigi <kakimomokuri@gmail.com>
2024-10-03 09:59:13 +0900

    build:フロントエンドセットアップ
    Add new libraries for testing and Babel updates
    
    This commit integrates several new development dependencies needed for testing, including `@testing-library` libraries and `jest-dom`. Additionally, it updates Babel-related dependencies to version `7.25.7`, ensuring compatibility and optimization.

---
+++ app/jest.config.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+    moduleFileExtensions: [
+        "js",
+        "ts",
+        "tsx"
+    ],
+    testMatch: [
+        "**/src/**/*.test.ts",
+        "**/src/**/*.test.tsx"
+    ],
+    roots: [
+        "<rootDir>/src"
+    ],
+    preset: "ts-jest",
+    testEnvironment: "jest-environment-jsdom",
+    transform: {
+        '^.+\\.jsx?$': 'babel-jest',
+        '^.+\\.tsx?$': 'babel-jest',
+        '\\.css$': 'jest-transform-css',
+    },
+    moduleNameMapper: {
+        '\\.(css|scss)$': 'identity-obj-proxy',
+        "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
+        uuid: require.resolve('uuid'),
+    },
+};
