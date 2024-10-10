95c45b4695e12f6dda656e03e96318499d58db65
Katuyuki Kakigi <kakimomokuri@gmail.com>
2024-10-10 10:22:52 +0900

    docs:開発
    Refactor UI and update authentication documentation
    
    Revise UI elements to represent login and home screens for better clarity and user experience. Update documentation to reflect changes in authentication specifications and associated models, use cases, and UI interactions.

--- index.js
+++ index.js
@@ -2,84 +2,81 @@ import "./style.css";
 import render from "@k2works/full-stack-lab";
 
 const contents = `
-## 機能名
+## 認証機能
+
 ## 仕様
+
+- ユーザーIDとパスワードで認証されたユーザーはホーム画面に遷移する
+- 登録されていないユーザーは認証されない
+- 管理者権限を持つユーザーは管理者として認証される
+
 ## TODOリスト
+- [x] ユーザーDBを作成する
+- [x] 認証APIを作成する
+- [ ] ログイン画面を作成する
+- [ ] ホーム画面を作成する
 `;
 
 const mindmap = `
 @startmindmap
-+ root
-++ right
-+++ right right
-*** right2
--- left
---- left left
--- left2
-
+* 認証
+-- ユースケース
+--- ユーザーを認証する
+--- 管理者を認証する
+-- オブジェクトモデル
+--- 認証APIを作成する
+-- データモデル
+--- ユーザーDBを作成する
+** ユーザーインターフェース
+*** ログイン画面を作成する
+*** ホーム画面を作成する
+** モデル
+** インタラクション
 @endmindmap
 `;
 
 const usecase = `
 @startuml
 left to right direction
-actor "Actor" as ac
-rectangle Application {
-  usecase "UseCase1" as UC1
-  usecase "UseCase2" as UC2
-  usecase "UseCase3" as UC3
+actor "ユーザー" as member
+actor "管理者" as admin
+rectangle ユーザー認証 {
+  usecase "管理者を認証する" as UC1
+  usecase "ユーザーを認証する" as UC2
 }
-ac --> UC1
-ac --> UC2
-ac --> UC3
+admin --> UC1
+member --> UC2
 @enduml
 `;
 
 const ui = `
 @startsalt
 {+
-  コレクション画面
+  ログイン画面
   {+
-  {
-  生徒
-  教員
-  組
-  部
-  イベント
-  } |
-  {
-    == 生徒
-    { + <&zoom-in> (          )}
-    {T#
-    + 田尻　智裕  | 3年B組    | 野球部 写真部
-    + 山田　太郎  | 3年A組    | 野球部
-    + 鈴木　花子  | 3年A組    | 写真部
+    {
+      [  ログイン   ]
+      ---------------------
+      ユーザーID    | "MyName   "
+      パスワード | "****     "
     }
-  }
   }
 ----------------
-  シングル画面
+  ホーム画面
   {+
   {
-  生徒
-  教員
-  組
-  部
-  イベント
+  ホーム
+  ログアウト
   } |
   {
     {
-      <&person> <b>田尻 智裕
+      <b>お知らせ
     }
+    ---------------------
     {
-      名前
-      田尻　智裕
-      組
-      3年B組
-      部
-      野球部 写真部
-      関連する生徒
-      田尻　智裕 山田　太郎　鈴木　花子
+        2020/01/01 お知らせ内容
+        2020/01/02 お知らせ内容
+        2020/01/03 お知らせ内容
     }
   }
   }
@@ -89,85 +86,48 @@ const ui = `
 
 const uiModel = `
 @startuml
-  class 部 {
-    名称
-    カテゴリー
-    生徒数
-    印刷()
-    新規()
-    削除()
+  class ログイン {
+    ユーザーID
+    パスワード
+    ログイン()
   }
-  class 生徒 {
-    氏名
-    成績
-    印刷()
-    新規()
-    削除()
+  
+  class ホーム {
+    お知らせ
   }
-  class 組 {
-    名称
-    印刷()
-    新規()
-    削除()
+  
+  class ナビゲーション {
+    ログイン()
+    ホーム()
   }
-  class 教員 {
-    氏名
-    電話番号
-    印刷()
-    新規()
-    削除()
-  }
-  class イベント {
-    名称
-    日付
-    印刷()
-    新規()
-    削除()
-  }
-  部 *-* 生徒
-  部 *-- 教員
-  イベント *- 教員
-  生徒 --* 組
+  
+  ホーム *- ナビゲーション 
 `;
 
 const uiInteraction = `
 @startuml
-  イベント_コレクション --> イベント_シングル
-  イベント_シングル --> 教員_シングル
-  教員_コレクション --> 教員_シングル
-  教員_シングル --> 部_コレクション
-  教員_シングル <-> 組_シングル
-  組_コレクション --> 組_シングル
-  組_シングル --> 生徒_コレクション
-  生徒_コレクション --> 生徒_シングル
-  生徒_シングル -> 組_シングル
-  生徒_シングル --> 部_コレクション
-  部_コレクション --> 部_シングル
-  部_シングル --> 生徒_コレクション
+  ログイン_シングル --> ホーム_シングル
+  ホーム_シングル --> ログイン_シングル 
 @enduml
 `;
 
 const uml = `
 @startuml
-abstract class AbstractList
-abstract AbstractCollection
-interface List
-interface Collection
-List <|-- AbstractList
-Collection <|-- AbstractCollection
-Collection <|- List
-AbstractCollection <|- AbstractList
-AbstractList <|-- ArrayList
-class ArrayList {
-  Object[] elementData
-  size()
+class ユーザー一覧
+class ユーザー
+class ユーザーID
+class パスワード
+class 役割
+class 名前 {
+    + 姓
+    + 名
 }
-enum TimeUnit {
-  DAYS
-  HOURS
-  MINUTES
-}
-annotation SuppressWarnings
+
+ユーザー一覧 *- ユーザー
+ユーザー *-- ユーザーID
+ユーザー *-- パスワード
+ユーザー *-- 役割
+ユーザー *-- 名前
 @enduml
 `;
 
@@ -177,26 +137,14 @@ const erd = `
 hide circle
 ' avoid problems with angled crows feet
 skinparam linetype ortho
-entity "Entity01" as e01 {
-  *e1_id : number <<generated>>
-  --
-  *name : text
-  description : text
+entity "ユーザー" as usr {
+    *ユーザーID : text
+    --
+    姓 : text
+    名 : text
+    パスワード : text
+    役割 : text
 }
-entity "Entity02" as e02 {
-  *e2_id : number <<generated>>
-  --
-  *e1_id : number <<FK>>
-  other_details : text
-}
-entity "Entity03" as e03 {
-  *e3_id : number <<generated>>
-  --
-  e1_id : number <<FK>>
-  other_details : text
-}
-e01 ||..o{ e02
-e01 |o..o{ e03
 @enduml
 `;
 
