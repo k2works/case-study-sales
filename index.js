2f0fb8db68484d27abdf63340c5634c923016728
Katuyuki Kakigi <kakimomokuri@gmail.com>
2024-10-12 11:39:48 +0900

    docs:開発
    Refactor user management to master management
    
    Replaced all instances of "ユーザー管理" with "マスタ管理" across various components including UI, UML diagrams, and data models. Updated the application header to "販売管理アプリケーション Ver0.1.0".

--- index.js
+++ index.js
@@ -1,155 +1,47 @@
 import "./style.css";
 import render from "@k2works/full-stack-lab";
 
+const mindmap = `
+@startmindmap
+* マスタ管理
+-- ユースケース
+-- ドメインモデル
+-- データモデル
+** ユーザーインターフェース
+*** ビュー
+*** モデル
+*** インタラクション
+@endmindmap
+`;
+
 const contents = `
-## ユーザー管理機能
+## マスタ管理機能
 
 ## 仕様
 
-- 管理者はユーザーを管理できる
+- 管理者はマスタを管理できる
 
 ## TODOリスト
-- [x] ユーザー管理APIを作成する
-- [x] ユーザー管理画面を作成する
-`;
-
-const mindmap = `
-@startmindmap
-* ユーザー管理
--- ユースケース
---- ユーザー一覧を取得する
---- ユーザーを新規登録する
---- ユーザーを取得する
---- 登録済みユーザーを更新登録する
---- 登録済みユーザーを削除する
--- ドメインモデル
---- ユーザー管理APIを作成する
--- データモデル
---- ユーザーDBを作成する
-** ユーザーインターフェース
-*** ユーザー管理画面を作成する
-** モデル
-*** ログイン
-*** ナビゲーション
-*** ユーザー一覧
-**** ユーザー
-** インタラクション
-@endmindmap
+- [ ] マスタ管理APIを作成する
+- [ ] マスタ管理画面を作成する
 `;
 
 const usecase = `
 @startuml
 left to right direction
 actor "管理者" as admin
-rectangle ユーザー管理 {
-    usecase "ユーザー一覧を取得する" as UC1
-    usecase "ユーザーを新規登録する" as UC2
-    usecase "ユーザーを取得する" as UC3
-    usecase "登録済みユーザーを更新登録する" as UC4
-    usecase "登録済みユーザーを削除する" as UC5
+rectangle マスタ管理 {
 }
-admin --> UC1
-admin --> UC2
-admin --> UC3
-admin --> UC4
-admin --> UC5
-@enduml
-`;
-
-const ui = `
-@startsalt
-{+
-  ユーザー管理画面（コレクション）
-  {+
-  {
-  ホーム
-  ユーザー
-  ログアウト
-  } |
-  {
-    {
-      <b>ユーザー一覧</b>
-    }
-    [  新規登録  ]
-    ---------------------
-    {
-      Uxxxxxx | User1    | [  編集  ] | [  削除  ]
-      Uxxxxxx | User2    | [  編集  ] | [  削除  ]
-      Uxxxxxx | User3    | [  編集  ] | [  削除  ]
-    }
-  }
-  }
-  ----------------
-    ユーザー管理画面（シングル）
-    {+
-        {
-        [  保存   ]
-        ---------------------
-        ユーザーID    | "Uxxxxxx"   "
-        姓           | "MyName   "
-        名           | "MyName   "
-        パスワード | "****     "
-        役割       | ^Admin     ^^User    ^
-        }
-    }
-}
-@endsalt
-`;
-
-const uiModel = `
-@startuml
-  class ユーザー一覧 {
-    新規作成()
-    編集()
-    削除()
-  }
-
-  class ユーザー {
-    ユーザーID
-    パスワード
-    役割
-    姓
-    名
-    保存()
-  }
-  
-  class ナビゲーション {
-    ホーム()
-    ユーザー()
-    ログアウト()
-  }
-  
-  ユーザー一覧 *-- ユーザー
-  ナビゲーション -* ユーザー一覧
-@enduml
-`;
-
-const uiInteraction = `
-@startuml
-  ログイン_シングル --> ユーザー_コレクション
-    ユーザー_コレクション --> ユーザー_シングル
-    ユーザー_シングル --> ユーザー_コレクション
-  ログイン_シングル <-- ユーザー_コレクション
+admin --> (マスタ管理)
 @enduml
 `;
 
 const uml = `
 @startuml
-class ユーザー一覧
-class ユーザー
-class ユーザーID
-class パスワード
-class 役割
-class 名前 {
-    + 姓
-    + 名
-}
+class マスタ一覧
+class マスタ
 
-ユーザー一覧 *- ユーザー
-ユーザー *-- ユーザーID
-ユーザー *-- パスワード
-ユーザー *-- 役割
-ユーザー *-- 名前
+マスタ一覧 *- マスタ
 @enduml
 `;
 
@@ -159,14 +51,57 @@ const erd = `
 hide circle
 ' avoid problems with angled crows feet
 skinparam linetype ortho
-entity "ユーザー" as usr {
-    *ユーザーID : text
+entity "マスタ" as master {
+    *ID : text
     --
-    姓 : text
-    名 : text
-    パスワード : text
-    役割 : text
 }
+@enduml
+`;
+
+const ui = `
+@startsalt
+{+
+  マスタ管理画面（コレクション）
+  {+
+  }
+  ----------------
+  マスタ管理画面（シングル）
+  {+
+  }
+}
+@endsalt
+`;
+
+const uiModel = `
+@startuml
+  class マスタ一覧 {
+    新規作成()
+    編集()
+    削除()
+  }
+
+  class マスタ {
+    保存()
+  }
+  
+  class ナビゲーション {
+    ホーム()
+    マスタ()
+    ユーザー()
+    ログアウト()
+  }
+  
+  ユーザー一覧 *-- マスタ
+  ナビゲーション -* マスタ一覧
+@enduml
+`;
+
+const uiInteraction = `
+@startuml
+  ログイン_シングル --> マスタ_コレクション
+    マスタ_コレクション --> マスタ_シングル
+    マスタ_シングル --> マスタ_コレクション
+  ログイン_シングル <-- マスタ_コレクション
 @enduml
 `;
 
