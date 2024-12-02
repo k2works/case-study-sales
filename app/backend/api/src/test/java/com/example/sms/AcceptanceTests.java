97ea0c7d5d588c157c6c6f7b3dbee548397c8836
k2works <kakimomokuri@gmail.com>
2024-12-02 14:16:36 +0900

    chore:レポート
    Integrate Allure for enhanced test reporting
    
    This commit introduces Allure to both backend and frontend for superior test report generation. Updates include configuring Allure in `docker-compose.yml`, adding necessary dependencies, and modifying build scripts. Additionally, allure results are now excluded via `.gitignore`, and a new browser link to allure reports is added in the `index.html`.

--- app/backend/api/src/test/java/com/example/sms/AcceptanceTests.java
+++ app/backend/api/src/test/java/com/example/sms/AcceptanceTests.java
@@ -7,7 +7,10 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "classpath:features/",
-        glue = {"com.example.sms"}
+        glue = {"com.example.sms"},
+        plugin = {
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        }
 )
 public class AcceptanceTests {
 }
