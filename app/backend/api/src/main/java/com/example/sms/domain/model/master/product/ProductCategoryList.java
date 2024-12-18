9613762244c7787b77d62beb3812c57c455e728d
k2works <kakimomokuri@gmail.com>
2024-12-18 14:21:52 +0900

    refactor:ファーストクラスコレクション
    Make ProductCategoryList immutable and add new item support
    
    Mark the internal list as unmodifiable to ensure immutability and added a method to create a new instance with an additional ProductCategory. This enhances data integrity and simplifies handling of the list's state.

--- app/backend/api/src/main/java/com/example/sms/domain/model/master/product/ProductCategoryList.java
+++ app/backend/api/src/main/java/com/example/sms/domain/model/master/product/ProductCategoryList.java
@@ -1,5 +1,7 @@
 package com.example.sms.domain.model.master.product;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -9,11 +11,17 @@ public class ProductCategoryList {
     List<ProductCategory> value;
 
     public ProductCategoryList(List<ProductCategory> value) {
-        this.value = value;
+        this.value = Collections.unmodifiableList(value);
     }
 
     public int size() {
         return value.size();
+    }
+
+    public ProductCategoryList add(ProductCategory productCategory) {
+        List<ProductCategory> newValue = new ArrayList<>(value);
+        newValue.add(productCategory);
+        return new ProductCategoryList(newValue);
     }
 
     public List<ProductCategory> asList() {
