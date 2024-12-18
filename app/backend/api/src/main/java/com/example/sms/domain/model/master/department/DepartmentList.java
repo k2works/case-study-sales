6a6c4b0e2d07195eb81c41f6019aeaa374fea1bf
k2works <kakimomokuri@gmail.com>
2024-12-18 14:19:08 +0900

    refactor:ファーストクラスコレクション
    Make list fields immutable and add 'add' methods.
    
    Wrapped list fields in `Collections.unmodifiableList` to ensure immutability. Introduced `add` methods in `ProductList`, `DepartmentList`, and `EmployeeList` to return new instances with added elements, preserving immutability principles.

--- app/backend/api/src/main/java/com/example/sms/domain/model/master/department/DepartmentList.java
+++ app/backend/api/src/main/java/com/example/sms/domain/model/master/department/DepartmentList.java
@@ -1,5 +1,7 @@
 package com.example.sms.domain.model.master.department;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -9,11 +11,17 @@ public class DepartmentList {
     List<Department> value;
 
     public DepartmentList(List<Department> value) {
-        this.value = value;
+        this.value = Collections.unmodifiableList(value);
     }
 
     public int size() {
         return value.size();
+    }
+
+    public DepartmentList add(Department department) {
+        List<Department> newValue = new ArrayList<>(value);
+        newValue.add(department);
+        return new DepartmentList(newValue);
     }
 
     public List<Department> asList() {
