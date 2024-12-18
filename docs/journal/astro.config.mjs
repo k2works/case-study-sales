e07fef364632be152c7dc4e7a88870b75647a450
k2works <kakimomokuri@gmail.com>
2024-12-18 15:27:35 +0900

    docs:エンジニアリング日誌
    Add new dependencies to package-lock.json for tooling
    
    This commit updates the package-lock.json file by adding several new dependencies, including Playwright, Astro plugins, Expressive Code plugins, D3 types, and others. These packages are likely introduced to enhance development tooling and functionalities in the project. The change supports improved workflows, additional functionality, or upgraded features.

--- docs/journal/astro.config.mjs
+++ docs/journal/astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import expressiveCode from "astro-expressive-code";
+import remarkMermaid from 'remark-mermaidjs'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    markdown: {
+        // Applied to .md and .mdx files
+        remarkPlugins: [remarkMermaid],
+    },
+    integrations: [expressiveCode(), mdx()]
 });
