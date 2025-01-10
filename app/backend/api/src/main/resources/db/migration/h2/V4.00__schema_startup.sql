bf5f91e9c1ef4513c9b0a54ac1318d92fbd0785d
k2works <kakimomokuri@gmail.com>
2025-01-10 18:47:24 +0900

    refactor:マイグレーション
    Refactor and reorganize H2 database schema.
    
    Reorganized database schema files by splitting definitions between versions and adding a new `V4` schema. Included sample data in `V4.01__data_startup.sql` to facilitate initial testing and setup. Removed redundant definitions from older schema files to ensure consistency.

---
+++ app/backend/api/src/main/resources/db/migration/h2/V4.00__schema_startup.sql
@@ -0,0 +1,195 @@
+create table if not exists 取引先マスタ
+(
+    取引先コード         varchar(8)                        not null
+    constraint pk_companys_mst
+    primary key,
+    取引先名             varchar(40)                       not null,
+    取引先名カナ         varchar(40),
+    仕入先区分           integer      default 0,
+    郵便番号             char(8),
+    都道府県             varchar(4),
+    住所１                varchar(40),
+    住所２                varchar(40),
+    取引禁止フラグ       integer      default 0,
+    雑区分               integer      default 0,
+    取引先グループコード varchar(4)                        not null,
+    与信限度額           integer      default 0,
+    与信一時増加枠       integer      default 0,
+    作成日時             timestamp(6) default CURRENT_DATE not null,
+    作成者名             varchar(12),
+    更新日時             timestamp(6) default CURRENT_DATE not null,
+    更新者名             varchar(12)
+    );
+
+create table if not exists 顧客マスタ
+(
+    顧客コード         varchar(8)                        not null
+    references 取引先マスタ
+    on update cascade on delete restrict,
+    顧客枝番           integer                           not null,
+    顧客区分           integer      default 0,
+    請求先コード       varchar(8)                        not null,
+    請求先枝番         integer,
+    回収先コード       varchar(8)                        not null,
+    回収先枝番         integer,
+    顧客名             varchar(40)                       not null,
+    顧客名カナ         varchar(40),
+    自社担当者コード   varchar(10)                       not null,
+    顧客担当者名       varchar(20),
+    顧客部門名         varchar(40),
+    顧客郵便番号       char(8),
+    顧客都道府県       varchar(4),
+    顧客住所１          varchar(40),
+    顧客住所２          varchar(40),
+    顧客電話番号       varchar(13),
+    "顧客ＦＡＸ番号"      varchar(13),
+    顧客メールアドレス varchar(100),
+    顧客請求区分       integer      default 0,
+    顧客締日１          integer                           not null,
+    顧客支払月１        integer      default 1,
+    顧客支払日１        integer,
+    顧客支払方法１      integer      default 1,
+    顧客締日２          integer                           not null,
+    顧客支払月２        integer      default 1,
+    顧客支払日２        integer,
+    顧客支払方法２      integer      default 1,
+    作成日時           timestamp(6) default CURRENT_DATE not null,
+    作成者名           varchar(12),
+    更新日時           timestamp(6) default CURRENT_DATE not null,
+    更新者名           varchar(12),
+    constraint pk_customer
+    primary key (顧客コード, 顧客枝番)
+    );
+
+comment on column 顧客マスタ.顧客請求区分 is '1:都度請求 2:締請求';
+
+comment on column 顧客マスタ.顧客締日１ is '15:15日締め';
+
+comment on column 顧客マスタ.顧客支払月１ is '0:当月 1:翌月 2:翌々月';
+
+comment on column 顧客マスタ.顧客支払日１ is '10:10日払い 99:末日';
+
+comment on column 顧客マスタ.顧客支払方法１ is '1:振込 2:手形';
+
+comment on column 顧客マスタ.顧客締日２ is '15:15日締め';
+
+comment on column 顧客マスタ.顧客支払月２ is '0:当月 1:翌月 2:翌々月';
+
+comment on column 顧客マスタ.顧客支払日２ is '10:10日払い 99:末日';
+
+comment on column 顧客マスタ.顧客支払方法２ is '1:振込 2:手形';
+
+create table if not exists 出荷先マスタ
+(
+    顧客コード     varchar(8)                        not null,
+    顧客枝番       integer                           not null,
+    出荷先番号     integer                           not null,
+    出荷先名       varchar(40)                       not null,
+    地域コード     varchar(10)                       not null,
+    出荷先郵便番号 char(8),
+    出荷先住所１    varchar(40),
+    出荷先住所２    varchar(40),
+    作成日時       timestamp(6) default CURRENT_DATE not null,
+    作成者名       varchar(12),
+    更新日時       timestamp(6) default CURRENT_DATE not null,
+    更新者名       varchar(12),
+    constraint pk_destinations
+    primary key (顧客コード, 出荷先番号, 顧客枝番),
+    foreign key (顧客コード, 顧客枝番) references 顧客マスタ
+    on update cascade on delete restrict
+    );
+
+create table if not exists 仕入先マスタ
+(
+    仕入先コード         varchar(8)                        not null
+    references 取引先マスタ
+    on update cascade on delete restrict,
+    仕入先枝番           integer                           not null,
+    仕入先名             varchar(40)                       not null,
+    仕入先名カナ         varchar(40),
+    仕入先担当者名       varchar(20),
+    仕入先部門名         varchar(40),
+    仕入先郵便番号       char(8),
+    仕入先都道府県       varchar(4),
+    仕入先住所１          varchar(40),
+    仕入先住所２          varchar(40),
+    仕入先電話番号       varchar(13),
+    "仕入先ＦＡＸ番号"      varchar(13),
+    仕入先メールアドレス varchar(100),
+    仕入先締日           integer                           not null,
+    仕入先支払月         integer      default 1,
+    仕入先支払日         integer,
+    仕入先支払方法       integer      default 1,
+    作成日時             timestamp(6) default CURRENT_DATE not null,
+    作成者名             varchar(12),
+    更新日時             timestamp(6) default CURRENT_DATE not null,
+    更新者名             varchar(12),
+    constraint pk_supplier
+    primary key (仕入先コード, 仕入先枝番)
+    );
+
+comment on column 仕入先マスタ.仕入先締日 is '15:15日締め';
+
+comment on column 仕入先マスタ.仕入先支払月 is '0:当月 1:翌月 2:翌々月';
+
+comment on column 仕入先マスタ.仕入先支払日 is '10:10日払い 99:末日';
+
+comment on column 仕入先マスタ.仕入先支払方法 is '1:振込 2:手形';
+
+create table if not exists 取引先グループマスタ
+(
+    取引先グループコード varchar(4)                        not null
+    constraint pk_company_group_mst
+    primary key,
+    取引先グループ名     varchar(40),
+    作成日時             timestamp(6) default CURRENT_DATE not null,
+    作成者名             varchar(12),
+    更新日時             timestamp(6) default CURRENT_DATE not null,
+    更新者名             varchar(12)
+    );
+
+create table if not exists 取引先分類種別マスタ
+(
+    取引先分類種別コード varchar(2)                        not null
+    constraint pk_category_type
+    primary key,
+    取引先分類種別名     varchar(20),
+    作成日時             timestamp(6) default CURRENT_DATE not null,
+    作成者名             varchar(12),
+    更新日時             timestamp(6) default CURRENT_DATE not null,
+    更新者名             varchar(12)
+    );
+
+create table if not exists 取引先分類マスタ
+(
+    取引先分類種別コード varchar(2)                        not null
+    references 取引先分類種別マスタ
+    on update cascade on delete restrict,
+    取引先分類コード     varchar(8)                        not null,
+    取引先分類名         varchar(30),
+    作成日時             timestamp(6) default CURRENT_DATE not null,
+    作成者名             varchar(12),
+    更新日時             timestamp(6) default CURRENT_DATE not null,
+    更新者名             varchar(12),
+    constraint pk_company_category
+    primary key (取引先分類種別コード, 取引先分類コード)
+    );
+
+create table if not exists 取引先分類所属マスタ
+(
+    取引先分類種別コード varchar(2)                        not null,
+    取引先分類コード     varchar(8)                        not null,
+    取引先コード         varchar(8)                        not null
+    references 取引先マスタ(取引先コード)
+    on update cascade on delete restrict,
+    作成日時             timestamp(6) default CURRENT_TIMESTAMP not null,
+    作成者名             varchar(12),
+    更新日時             timestamp(6) default CURRENT_TIMESTAMP not null,
+    更新者名             varchar(12),
+    constraint pk_company_category_group
+    primary key (取引先分類種別コード, 取引先コード, 取引先分類コード),
+    constraint 取引先分類所属マスタ_取引先分類コード__fkey
+    foreign key (取引先分類コード, 取引先分類種別コード) references 取引先分類マスタ(取引先分類コード, 取引先分類種別コード)
+    on update cascade on delete restrict
+    );
+
---
+++ app/backend/api/src/main/resources/db/migration/h2/V4.01__data_startup.sql
@@ -0,0 +1,52 @@
+-- 取引先グループマスタのサンプルデータ挿入
+INSERT INTO 取引先グループマスタ (取引先グループコード, 取引先グループ名, 作成日時, 作成者名, 更新日時, 更新者名)
+VALUES
+    ('0001', '得意先', CURRENT_TIMESTAMP, 'admin', CURRENT_TIMESTAMP, 'admin'),
+    ('0002', '仕入先', CURRENT_TIMESTAMP, 'admin', CURRENT_TIMESTAMP, 'admin');
+
+-- 取引先マスタのサンプルデータ
+insert into 取引先マスタ (取引先コード, 取引先名, 取引先名カナ, 仕入先区分, 郵便番号, 都道府県, 住所１, 住所２, 取引禁止フラグ, 雑区分, 取引先グループコード, 与信限度額, 与信一時増加枠, 作成日時, 作成者名, 更新日時, 更新者名)
+values
+    ('001', 'DBMフード株式会社', 'デービーエムフードカブシキガイシャ', 1, '123-4567', '東京都', '千代田区丸の内', 'ビル1F', 0, 0, '0001', 1000000, 100000, CURRENT_TIMESTAMP, '作成者A', CURRENT_TIMESTAMP, '更新者A'),
+    ('002', '株式会社B', 'カブシキガイシャビー', 0, '987-6543', '大阪府', '梅田1丁目', 'タワー2F', 0, 0, '0002', 500000, 50000, CURRENT_TIMESTAMP, '作成者B', CURRENT_TIMESTAMP, '更新者B'),
+    ('003', '株式会社C', 'カブシキガイシャシー', 0, '731-0001', '広島県', '広島市', '安佐南区', 0, 0, '0002', 500000, 50000, CURRENT_TIMESTAMP, '作成者B', CURRENT_TIMESTAMP, '更新者B');
+
+-- 顧客マスタのサンプルデータ
+insert into 顧客マスタ (顧客コード, 顧客枝番, 顧客区分, 請求先コード, 請求先枝番, 回収先コード, 回収先枝番, 顧客名, 顧客名カナ, 自社担当者コード, 顧客担当者名, 顧客部門名, 顧客郵便番号, 顧客都道府県, 顧客住所１, 顧客住所２, 顧客電話番号, 顧客ＦＡＸ番号, 顧客メールアドレス, 顧客請求区分, 顧客締日１, 顧客支払月１, 顧客支払日１, 顧客支払方法１, 顧客締日２, 顧客支払月２, 顧客支払日２, 顧客支払方法２, 作成日時, 作成者名, 更新日時, 更新者名)
+values
+    ('001', 1, 0, '001', null, '001', null, '東京本社', 'トウキョウホンシャ', 'T001', '田中太郎', '営業部', '123-4567', '東京都', '千代田区丸の内2丁目', 'ビル3F', '03-1234-5678', '03-1234-9876', 'tanaka@example.com', 2, 15, 1, 10, 1, 15, 1, 99, 1, CURRENT_TIMESTAMP, '作成者A', CURRENT_TIMESTAMP, '更新者A'),
+    ('001', 2, 0, '001', null, '001', null, '関西支社', 'カンサイシシャ', 'T001', '田中太郎', '営業部', '123-4567', '大阪府', '梅田1丁目', 'ビル1F', '03-1234-5678', '03-1234-9876', 'tanaka@example.com', 2, 15, 1, 10, 1, 15, 1, 99, 1, CURRENT_TIMESTAMP, '作成者A', CURRENT_TIMESTAMP, '更新者A');
+
+-- 出荷先マスタのサンプルデータ
+insert into 出荷先マスタ (顧客コード, 顧客枝番, 出荷先番号, 出荷先名, 地域コード, 出荷先郵便番号, 出荷先住所１, 出荷先住所２, 作成日時, 作成者名, 更新日時, 更新者名)
+values
+    ('001', 1, 1, '新宿営業所', 'R001', '123-4567', '新宿1丁目', '倉庫A区画', CURRENT_TIMESTAMP, '作成者A', CURRENT_TIMESTAMP, '更新者A'),
+    ('001', 1, 2, '横浜営業所', 'R001', '123-4567', '横浜2丁目', '倉庫B区画', CURRENT_TIMESTAMP, '作成者A', CURRENT_TIMESTAMP, '更新者A'),
+    ('001', 1, 3, '中之島営業所', 'R001', '123-4567', '千代田区丸の内2丁目', '倉庫区画', CURRENT_TIMESTAMP, '作成者A', CURRENT_TIMESTAMP, '更新者A');
+
+-- 仕入先マスタのサンプルデータ
+insert into 仕入先マスタ (仕入先コード, 仕入先枝番, 仕入先名, 仕入先名カナ, 仕入先担当者名, 仕入先部門名, 仕入先郵便番号, 仕入先都道府県, 仕入先住所１, 仕入先住所２, 仕入先電話番号, 仕入先ＦＡＸ番号, 仕入先メールアドレス, 仕入先締日, 仕入先支払月, 仕入先支払日, 仕入先支払方法, 作成日時, 作成者名, 更新日時, 更新者名)
+values
+    ('002', 1, '本社', 'ホンシャ', '山田花子', '仕入部', '987-6543', '大阪府', '吹田市1丁目', '工場1F', '06-5432-1234', '06-5432-9876', 'yamada@example.com', 15, 1, 10, 2, CURRENT_TIMESTAMP, '作成者B', CURRENT_TIMESTAMP, '更新者B');
+
+-- 取引先分類種別マスタへのサンプルデータ挿入
+INSERT INTO 取引先分類種別マスタ (取引先分類種別コード, 取引先分類種別名, 作成日時, 作成者名, 更新日時, 更新者名)
+VALUES
+    ('01', '業種', CURRENT_TIMESTAMP, 'admin', CURRENT_TIMESTAMP, 'admin'),
+    ('02', '業態', CURRENT_TIMESTAMP, 'admin', CURRENT_TIMESTAMP, 'admin'),
+    ('03', '規模', CURRENT_TIMESTAMP, 'admin', CURRENT_TIMESTAMP, 'admin');
+
+-- 取引先分類マスタへのサンプルデータ挿入
+INSERT INTO 取引先分類マスタ (取引先分類種別コード, 取引先分類コード, 取引先分類名, 作成日時, 作成者名, 更新日時, 更新者名)
+VALUES
+    ('02', '001', '小売', CURRENT_TIMESTAMP, 'admin', CURRENT_TIMESTAMP, 'admin'),
+    ('02', '002', '卸', CURRENT_TIMESTAMP, 'admin', CURRENT_TIMESTAMP, 'admin'),
+    ('02', '003', '製造', CURRENT_TIMESTAMP, 'admin', CURRENT_TIMESTAMP, 'admin'),
+    ('02', '004', 'サービス', CURRENT_TIMESTAMP, 'admin', CURRENT_TIMESTAMP, 'admin');
+
+-- 取引先分類所属マスタへのサンプルデータ挿入
+INSERT INTO 取引先分類所属マスタ (取引先分類種別コード, 取引先分類コード, 取引先コード, 作成日時, 作成者名, 更新日時, 更新者名)
+VALUES
+    ('02', '002', '001', CURRENT_TIMESTAMP, 'admin', CURRENT_TIMESTAMP, 'admin'),
+    ('02', '002', '002', CURRENT_TIMESTAMP, 'admin', CURRENT_TIMESTAMP, 'admin'),
+    ('02', '002', '003', CURRENT_TIMESTAMP, 'admin', CURRENT_TIMESTAMP, 'admin');
