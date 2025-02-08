287627c0dd623f66d1e602a90c9e288063f6b8e3
k2works <kakimomokuri@gmail.com>
2025-02-08 13:41:27 +0900

    refactor(WIP):コンテキストとカスタムフックの併用
    Refactor customer management with modularized components
    
    Introduced new components for customer modals (search, edit, and region selection) and revamped the `CustomerContainer` to utilize a cleaner, more modular structure using context providers. This improves readability, maintainability, and separates concerns across functionalities tied to customer management.

---
+++ app/frontend/src/components/master/partner/customer/CustomerCollection.tsx
@@ -0,0 +1,128 @@
+import React from "react";
+import {useCustomerContext} from "../../../../providers/Customer.tsx";
+import {CustomerCodeType, CustomerType} from "../../../../models/master/partner";
+import {showErrorMessage} from "../../../application/utils.ts";
+import {CustomerCollectionView} from "../../../../views/master/partner/customer/CustomerCollection.tsx";
+import {CustomerSearchModal} from "./CustomerSearchModal.tsx";
+import {CustomerEditModal} from "./CustomerEditModal.tsx";
+
+export const CustomerCollection: React.FC = () => {
+    const {
+        message,
+        setMessage,
+        error,
+        setError,
+        pageNation,
+        criteria,
+        setSearchModalIsOpen,
+        setModalIsOpen,
+        setIsEditing,
+        setEditId,
+        initialCustomer,
+        customers,
+        setCustomers,
+        setNewCustomer,
+        searchCustomerCriteria,
+        setSearchCustomerCriteria,
+        fetchCustomers,
+        customerService
+    } = useCustomerContext();
+
+    const handleOpenModal = (customer?: CustomerType) => {
+        setMessage("");
+        setError("");
+        if (customer) {
+            setNewCustomer(customer);
+            setEditId(customer.customerCode.code.value);
+            setIsEditing(true);
+        } else {
+            setNewCustomer(initialCustomer);
+            setIsEditing(false);
+        }
+        setModalIsOpen(true);
+    };
+
+    const handleOpenSearchModal = () => {
+        setSearchModalIsOpen(true);
+    };
+
+    const handleDeleteCustomer = async (customerCode: CustomerCodeType) => {
+        try {
+            if (!window.confirm(`顧客コード:${customerCode.code.value} 枝番:${customerCode.branchNumber} を削除しますか？`)) return;
+            await customerService.destroy(customerCode);
+            await fetchCustomers.load();
+            setMessage("顧客を削除しました。");
+        } catch (error: any) {
+            showErrorMessage(`顧客の削除に失敗しました: ${error?.message}`, setError);
+        }
+    };
+
+    const handleCheckCustomer = (customer: CustomerType) => {
+        const newCustomers = customers.map((c) => {
+            if (c.customerCode.code.value === customer.customerCode.code.value) {
+                return { ...c, checked: !c.checked };
+            }
+            return c;
+        });
+        setCustomers(newCustomers);
+    }
+
+    const handleCheckAllCustomers = () => {
+        const newCustomers = customers.map((c) => {
+            return { ...c, checked: !c.checked };
+        });
+        setCustomers(newCustomers);
+    }
+
+    const handleDeleteCheckCollection = async () => {
+        const checkedCustomers = customers.filter((c) => c.checked);
+        if (checkedCustomers.length === 0) {
+            showErrorMessage("削除する顧客を選択してください", setError);
+            return;
+        }
+        try {
+            if (!window.confirm("選択した顧客を削除しますか？")) return;
+            await Promise.all(
+                checkedCustomers.map((customer) =>
+                    customerService.destroy(customer.customerCode)
+                )
+            );
+            await fetchCustomers.load();
+            setMessage("選択した顧客を削除しました。");
+        } catch (error: any) {
+            showErrorMessage(`選択した顧客の削除に失敗しました: ${error?.message}`, setError);
+        }
+    }
+
+    return (
+        <>
+            <CustomerSearchModal/>
+            <CustomerEditModal/>
+            <CustomerCollectionView
+                error={error}
+                message={message}
+                searchItems={{
+                    searchCustomerCriteria,
+                    setSearchCustomerCriteria,
+                    handleOpenSearchModal,
+                }}
+                headerItems={{
+                    handleOpenModal,
+                    handleCheckToggleCollection: handleCheckAllCustomers,
+                    handleDeleteCheckedCollection: handleDeleteCheckCollection,
+                }}
+                collectionItems={{
+                    customers,
+                    handleOpenModal,
+                    handleDeleteCustomer,
+                    handleCheckCustomer,
+                }}
+                pageNationItems={{
+                    pageNation,
+                    fetchCustomers: fetchCustomers.load,
+                    criteria,
+                }}
+            />
+        </>
+    );
+}
