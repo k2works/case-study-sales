28d117ef1723f321d805c53f23df9cd67626e00a
k2works <kakimomokuri@gmail.com>
2025-02-08 12:29:55 +0900

    refactor(WIP):コンテキストとカスタムフックの併用
    Add PartnerGroup context and modularize related components
    
    Introduced `PartnerGroupContext` for centralized state management of the PartnerGroup module. Modularized components like search modal, edit modal, and collection view, improving reusability and maintainability. Updated `PartnerGroupContainer` to integrate the new structure.

---
+++ app/frontend/src/components/master/partner/group/PartnerGroupSearchModal.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {usePartnerGroupContext} from "../../../../providers/PartnerGroup.tsx";
+import Modal from "react-modal";
+import {PartnerGroupSearchView} from "../../../../views/master/partner/group/PartnerGroupSearch.tsx";
+import {showErrorMessage} from "../../../application/utils.ts";
+
+export const PartnerGroupSearchModal: React.FC = () => {
+    const {
+        setLoading,
+        setMessage,
+        setError,
+        setPageNation,
+        setCriteria,
+        searchModalIsOpen,
+        setSearchModalIsOpen,
+        setPartnerGroups,
+        searchPartnerGroupCriteria,
+        setSearchPartnerGroupCriteria,
+        partnerGroupService,
+    } = usePartnerGroupContext();
+
+    const handleCloseSearchModal = () => {
+        setSearchModalIsOpen(false);
+    };
+
+    const handleSelectSearchModal = async () => {
+        if (!searchPartnerGroupCriteria) return;
+
+        setLoading(true);
+        try {
+            const result = await partnerGroupService.search(searchPartnerGroupCriteria);
+            setPartnerGroups(result ? result.list : []);
+            if (result.list.length === 0) {
+                showErrorMessage("検索結果は0件です", setError);
+            } else {
+                setCriteria(searchPartnerGroupCriteria);
+                setPageNation(result);
+                setMessage("");
+                setError("");
+            }
+        } catch (error: any) {
+            showErrorMessage(`検索に失敗しました: ${error?.message}`, setError);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <Modal
+            isOpen={searchModalIsOpen}
+            onRequestClose={handleCloseSearchModal}
+            contentLabel="検索情報を入力"
+            className="modal"
+            overlayClassName="modal-overlay"
+            bodyOpenClassName="modal-open"
+        >
+            <PartnerGroupSearchView
+                criteria={searchPartnerGroupCriteria}
+                setCondition={setSearchPartnerGroupCriteria}
+                handleSelect={handleSelectSearchModal}
+                handleClose={handleCloseSearchModal}
+            />
+        </Modal>
+    );
+}
---
+++ app/frontend/src/components/master/partner/group/PartnerGroupSingle.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {usePartnerGroupContext} from "../../../../providers/PartnerGroup.tsx";
+import {showErrorMessage} from "../../../application/utils.ts";
+import {PartnerGroupSingleView} from "../../../../views/master/partner/group/PartnerGroupSingle.tsx";
+
+export const PartnerGroupSingle: React.FC = () => {
+    const {
+        message,
+        setMessage,
+        error,
+        setError,
+        setModalIsOpen,
+        isEditing,
+        editId,
+        setEditId,
+        initialPartnerGroup,
+        newPartnerGroup,
+        setNewPartnerGroup,
+        fetchPartnerGroups,
+        partnerGroupService,
+    } = usePartnerGroupContext();
+
+    const handleCloseModal = () => {
+        setError("");
+        setModalIsOpen(false);
+        setEditId(null);
+    };
+
+    const handleCreateOrUpdatePartnerGroup = async () => {
+        const validatePartnerGroup = (): boolean => {
+            if (!newPartnerGroup.partnerGroupCode.value.trim() ||
+                !newPartnerGroup.partnerGroupName.trim()) {
+                setError("取引先グループコードと名称は必須項目です。");
+                return false;
+            }
+            return true;
+        };
+
+        if (!validatePartnerGroup()) return;
+
+        try {
+            if (isEditing && editId) {
+                await partnerGroupService.update(newPartnerGroup);
+            } else {
+                await partnerGroupService.create(newPartnerGroup);
+            }
+            setNewPartnerGroup({ ...initialPartnerGroup });
+            await fetchPartnerGroups.load();
+            setMessage(isEditing ? "取引先グループを更新しました。" : "取引先グループを作成しました。");
+            handleCloseModal();
+        } catch (error: any) {
+            showErrorMessage(`取引先グループの作成または更新に失敗しました: ${error?.message}`, setError);
+        }
+    };
+
+    return (
+        <PartnerGroupSingleView
+            error={error}
+            message={message}
+            isEditing={isEditing}
+            headerItems={{ handleCreateOrUpdatePartnerGroup, handleCloseModal }}
+            formItems={{ newPartnerGroup, setNewPartnerGroup }}
+        />
+    );
+}
