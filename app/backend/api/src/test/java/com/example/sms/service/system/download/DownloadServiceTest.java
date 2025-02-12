459fa5ecc92d1b7d72d7d362156d5dbb91bb7ed8
k2works <kakimomokuri@gmail.com>
2025-02-12 15:35:55 +0900

    test:ダウンロード
    Add tests for data download and conversion functionality
    
    New test cases have been added to cover download and conversion logic for various entities like Product, Customer, and Supplier. These tests ensure correct data handling, validation of counts, and transformation to respective CSV formats. This improves coverage and reliability of the download service.

--- app/backend/api/src/test/java/com/example/sms/service/system/download/DownloadServiceTest.java
+++ app/backend/api/src/test/java/com/example/sms/service/system/download/DownloadServiceTest.java
@@ -5,9 +5,7 @@ import com.example.sms.TestDataFactory;
 import com.example.sms.domain.model.master.product.*;
 import com.example.sms.domain.model.system.download.*;
 import com.example.sms.domain.model.system.download.Product;
-import com.example.sms.infrastructure.datasource.system.download.DepartmentDownloadCSV;
-import com.example.sms.infrastructure.datasource.system.download.EmployeeDownloadCSV;
-import com.example.sms.infrastructure.datasource.system.download.ProductDownloadCSV;
+import com.example.sms.infrastructure.datasource.system.download.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -141,6 +139,165 @@ public class DownloadServiceTest {
             assertEquals(StockAllocationType.引当済.getCode(), result.getFirst().getInventoryAllocationCategory());
             assertEquals("009", result.getFirst().getVendorCode());
             assertEquals(9, result.getFirst().getSupplierBranchNumber());
+        }
+    }
+    @Nested
+    @DisplayName("商品")
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    class ProductDownload {
+        @Test
+        @DisplayName("件数取得")
+        void testCount() {
+            DownloadCriteria condition = Product.of();
+            int result = downloadService.count(condition);
+
+            assertEquals(3, result);
+        }
+
+        @Test
+        @DisplayName("データダウンロード変換")
+        void testDownload() {
+            DownloadCriteria condition = Product.of();
+            List<?> rawResult = downloadService.convert(condition);
+
+            List<ProductDownloadCSV> result = rawResult.stream()
+                    .filter(ProductDownloadCSV.class::isInstance)
+                    .map(ProductDownloadCSV.class::cast)
+                    .toList();
+
+            assertFalse(result.isEmpty());
+            assertEquals("99999999", result.getFirst().getProductCode());
+            assertEquals("商品1", result.getFirst().getProductFormalName());
+            assertEquals("商品1", result.getFirst().getProductAbbreviation());
+            assertEquals("ショウヒンイチ", result.getFirst().getProductNameKana());
+            assertEquals(ProductType.その他.getCode(), result.getFirst().getProductCategory());
+            assertEquals(900, result.getFirst().getSalesPrice().intValue());
+            assertEquals(810, result.getFirst().getPurchasePrice().intValue());
+            assertEquals(90, result.getFirst().getCostOfGoodsSold().intValue());
+            assertEquals(TaxType.その他.getCode(), result.getFirst().getTaxCategory());
+            assertEquals("カテゴリ9", result.getFirst().getProductCategoryCode());
+            assertEquals(MiscellaneousType.適用外.getCode(), result.getFirst().getMiscellaneousCategory());
+            assertEquals(StockManagementTargetType.対象.getCode(), result.getFirst().getInventoryManagementCategory());
+            assertEquals(StockAllocationType.引当済.getCode(), result.getFirst().getInventoryAllocationCategory());
+            assertEquals("009", result.getFirst().getVendorCode());
+            assertEquals(9, result.getFirst().getSupplierBranchNumber());
+        }
+    }
+    @Nested
+    @DisplayName("取引先グループ")
+    @WithMockUser(username = "XXXXX", roles = "ADMIN")
+    class PartnerGroupDownload {
+        @Test
+        @DisplayName("件数取得")
+        void testCount() {
+            DownloadCriteria condition = PartnerGroup.of();
+            int result = downloadService.count(condition);
+
+            assertEquals(2, result);
+        }
+
+        @Test
+        @DisplayName("データダウンロード変換")
+        void testDownload() {
+            DownloadCriteria condition = PartnerGroup.of();
+            List<?> rawResult = downloadService.convert(condition);
+
+            List<PartnerGroupDownloadCSV> result = rawResult.stream()
+                    .filter(PartnerGroupDownloadCSV.class::isInstance)
+                    .map(PartnerGroupDownloadCSV.class::cast)
+                    .toList();
+
+            assertFalse(result.isEmpty());
+            assertEquals("0001", result.getFirst().getPartnerGroupCode());
+            assertEquals("取引先グループ1", result.getFirst().getPartnerGroupName());
+        }
+    }
+    @Nested
+    @DisplayName("取引先")
+    @WithMockUser(username = "XXXXX", roles = "ADMIN")
+    class PartnerDownload {
+        @Test
+        @DisplayName("件数取得")
+        void testCount() {
+            DownloadCriteria condition = Partner.of();
+            int result = downloadService.count(condition);
+
+            assertEquals(3, result);
+        }
+
+        @Test
+        @DisplayName("データダウンロード変換")
+        void testDownload() {
+            DownloadCriteria condition = Partner.of();
+            List<?> rawResult = downloadService.convert(condition);
+
+            List<PartnerDownloadCSV> result = rawResult.stream()
+                    .filter(PartnerDownloadCSV.class::isInstance)
+                    .map(PartnerDownloadCSV.class::cast)
+                    .toList();
+
+            assertFalse(result.isEmpty());
+            assertEquals("PG01", result.getFirst().getPartnerGroupCode());
+            assertEquals("001", result.getFirst().getPartnerCode());
+            assertEquals("取引先名A", result.getFirst().getPartnerName());
+        }
+    }
+    @Nested
+    @DisplayName("顧客")
+    @WithMockUser(username = "XXXXX", roles = "ADMIN")
+    class CustomerDownload {
+        @Test
+        @DisplayName("件数取得")
+        void testCount() {
+            DownloadCriteria condition = Customer.of();
+            int result = downloadService.count(condition);
+
+            assertEquals(3, result);
+        }
+
+        @Test
+        @DisplayName("データダウンロード変換")
+        void testDownload() {
+            DownloadCriteria condition = Customer.of();
+            List<?> rawResult = downloadService.convert(condition);
+
+            List<CustomerDownloadCSV> result = rawResult.stream()
+                    .filter(CustomerDownloadCSV.class::isInstance)
+                    .map(CustomerDownloadCSV.class::cast)
+                    .toList();
+
+            assertFalse(result.isEmpty());
+            assertEquals("001", result.getFirst().getCustomerCode());
+            assertEquals("山田太郎", result.getFirst().getCustomerName());
+        }
+    }
+    @Nested
+    @DisplayName("仕入先")
+    @WithMockUser(username = "XXXXX", roles = "ADMIN")
+    class SupplierDownload {
+        @Test
+        @DisplayName("件数取得")
+        void testCount() {
+            DownloadCriteria condition = Vendor.of();
+            int result = downloadService.count(condition);
+
+            assertEquals(3, result);
+        }
+
+        @Test
+        @DisplayName("データダウンロード変換")
+        void testDownload() {
+            DownloadCriteria condition = Vendor.of();
+            List<?> rawResult = downloadService.convert(condition);
+
+            List<VendorDownloadCSV> result = rawResult.stream()
+                    .filter(VendorDownloadCSV.class::isInstance)
+                    .map(VendorDownloadCSV.class::cast)
+                    .toList();
+
+            assertFalse(result.isEmpty());
+            assertEquals("002", result.getFirst().getVendorCode());
+            assertEquals("仕入先名A", result.getFirst().getVendorName());
         }
     }
 }
