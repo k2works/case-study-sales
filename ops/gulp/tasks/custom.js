73c78368ea80fd6d867e0fad9f6911155d790bbe
k2works <kakimomokuri@gmail.com>
2025-02-13 10:26:34 +0900

    docs:スナップショットの作成
    Add release documentation assets for version 0.4.0
    
    Added new assets including Excel files, favicon, and package relation diagrams for version 0.4.0 release. These files provide visual and tabular documentation updates for the release.

--- ops/gulp/tasks/custom.js
+++ ops/gulp/tasks/custom.js
@@ -217,6 +217,12 @@ const assets = {
                     <img src="./v0_3_0/jig-erd/library-er-summary.svg" alt="Summary">
                 </div>
                 
+                <h2>v0.4.0</h2>
+                <div>
+                    <p><a href="./v0_4_0/jig/index.html">JIG</a></p>
+                    <img src="./v0_4_0/jig-erd/library-er-summary.svg" alt="Summary">
+                </div>
+                
                 <p>Powered by <a href="https://github.com/dddjava/jig">Jig</a> </p>
            </body>
         </html>
