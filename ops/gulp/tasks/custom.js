ff936e7171ec850529116739d64d6a348eefd6bf
k2works <kakimomokuri@gmail.com>
2025-08-04 18:22:17 +0900

    docs:リリース

--- ops/gulp/tasks/custom.js
+++ ops/gulp/tasks/custom.js
@@ -241,6 +241,12 @@ const assets = {
                     <img src="./v0_7_0/jig-erd/library-er-summary.svg" alt="Summary">
                 </div>
                 
+                <h2>v0.8.0</h2>
+                <div>
+                    <p><a href="./v0_8_0/jig/index.html">JIG</a></p>
+                    <img src="./v0_8_0/jig-erd/library-er-summary.svg" alt="Summary">
+                </div>
+                
                 <p>Powered by <a href="https://github.com/dddjava/jig">Jig</a> </p>
            </body>
         </html>
